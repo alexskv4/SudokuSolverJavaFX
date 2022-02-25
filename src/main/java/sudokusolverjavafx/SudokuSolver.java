c141536714fe935bc8a6668ad5d250e65760163b
alexskv4 <alexskv4@gmail.com>
2022-02-25 11:07:01 -0800

    Removed unnecessary parameters from boardCheck function.

--- src/main/java/sudokusolverjavafx/SudokuSolver.java
+++ src/main/java/sudokusolverjavafx/SudokuSolver.java
@@ -61,7 +61,7 @@ public class SudokuSolver {
                 if (board[i][j] == '.'){ //Check if empty cell
                     for (int k =1; k<= 9; k++){ //iterate trying to put every integer into cell
                         board[i][j] = (char)(k + '0');
-                        if (boardCheck(board[i], board[i][j], j, i, board)){
+                        if (boardCheck(j, i, board)){
                             solveSudoku(board);
                             if (solved){
                                 return board;
@@ -85,15 +85,15 @@ public class SudokuSolver {
         return board;
     }
 
-    public boolean boardCheck(char[] row, char num, int numIndex, int rowIndex, char[][] board){
+    public boolean boardCheck(int numIndex, int rowIndex, char[][] board){
 
         // iterate over both the vertical and horizontal and check if it is unique.
         int xCounter = 0;
         int yCounter = 0;
 
 
-        for (char digit : row){ // Checking if number is unique in row.
-            if (digit == num){
+        for (char digit : board[rowIndex]){ // Checking if number is unique in row.
+            if (digit == board[rowIndex][numIndex]){
                 xCounter++;
             }
         }
@@ -102,7 +102,7 @@ public class SudokuSolver {
         }
 
         for (int i = 0; i <= 8; i++){  // Checking if number is unique in column.
-            if (board[i][numIndex] == num){
+            if (board[i][numIndex] == board[rowIndex][numIndex]){
                 yCounter++;
             }
         }
@@ -127,65 +127,3 @@ public class SudokuSolver {
         return square.size() == new HashSet<>(square).size();
     }
 }
-
-//class Main {
-//    public static void main(String[] args) {
-//
-//        char[][] board1 = {
-//                {'5','3','.','.','7','.','.','.','.'},
-//                {'6','.','.','1','9','5','.','.','.'},
-//                {'.','9','8','.','.','.','.','6','.'},
-//                {'8','.','.','.','6','.','.','.','3'},
-//                {'4','.','.','8','.','3','.','.','1'},
-//                {'7','.','.','.','2','.','.','.','6'},
-//                {'.','6','.','.','.','.','2','8','.'},
-//                {'.','.','.','4','1','9','.','.','5'},
-//                {'.','.','.','.','8','.','.','7','9'}
-//        };
-//
-//        char[][] board2 = {
-//                {'.','.','9','7','4','8','.','.','.'},
-//                {'7','.','.','.','.','.','.','.','.'},
-//                {'.','2','.','1','.','9','.','.','.'},
-//                {'.','.','7','.','.','.','2','4','.'},
-//                {'.','6','4','.','1','.','5','9','.'},
-//                {'.','9','8','.','.','.','3','.','.'},
-//                {'.','.','.','8','.','3','.','2','.'},
-//                {'.','.','.','.','.','.','.','.','6'},
-//                {'.','.','.','2','7','5','9','.','.'}
-//        };
-//
-//        char[][] board3 = {
-//                {'.','.','4','6','9','2','7','3','1'},
-//                {'3','1','6','8','5','7','9','2','4'},
-//                {'9','2','7','1','3','4','6','8','5'},
-//                {'6','4','1','2','8','9','5','7','3'},
-//                {'8','9','2','5','7','3','4','1','6'},
-//                {'7','5','3','4','1','6','8','9','2'},
-//                {'1','6','5','9','2','8','3','4','7'},
-//                {'4','7','9','3','6','1','2','5','8'},
-//                {'2','3','8','7','4','5','1','6','9'}
-//        };
-//
-//        char[][] board4 = {
-//                {'.','.','.','.','.','.','.','.','.'},
-//                {'.','.','.','.','.','.','.','.','.'},
-//                {'.','.','.','.','.','.','.','.','.'},
-//                {'.','.','.','.','.','.','.','.','.'},
-//                {'.','.','.','.','.','.','.','.','.'},
-//                {'.','.','.','.','.','.','.','.','.'},
-//                {'.','.','.','.','.','.','.','.','.'},
-//                {'.','.','.','.','.','.','.','.','.'},
-//                {'.','.','.','.','.','.','.','.','.'}
-//        };
-//
-//        SudokuSolver sudokuSolver = new SudokuSolver();
-//
-//        char[][] correctBoard = sudokuSolver.solveSudoku(board1);
-//
-//        for (int i = 0; i < 9; i++) {
-//            System.out.println(Arrays.toString(correctBoard[i]));
-//
-//        }
-//    }
-//}
