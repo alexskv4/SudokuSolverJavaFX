c8f747dcd5be4151e16393b73f020e6efdb0b562
alexskv4 <alexskv4@gmail.com>
2022-02-27 09:59:41 -0800

    Custom sudokuCell mostly finished, ready to be implemented.

--- src/main/java/sudokusolverjavafx/SudokuCell.java
+++ src/main/java/sudokusolverjavafx/SudokuCell.java
@@ -1,31 +1,29 @@
 package sudokusolverjavafx;
 
 import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
+import javafx.scene.input.KeyCode;
 import javafx.scene.text.Font;
 
-public class SudokuCell extends Pane {
+public class SudokuCell extends Label {
 
     SudokuCell(){
 
         getStylesheets().add(Resources.get("SudokuCell.css"));
-
-        setFocusTraversable(true);
-        Label label = new Label("Placeholder text");
-        label.setFont(new Font(40));
+        setFont(new Font(40));
+        setText("");
 
         setOnKeyPressed(event -> {
             if (event.getCode().isDigitKey()){
-                label.setText(event.getText());
+                setText(event.getText());
+            }
+            else if(event.getCode() == KeyCode.BACK_SPACE){
+                setText("");
             }
 
         });
 
+        focusedProperty().addListener((obs, oldVal, newVal) -> setStyle(newVal? "-fx-background-color: orange" : "-fx-background-color: lightblue" ));
         setOnMouseClicked(event -> requestFocus());
 
-        getChildren().add(label);
-
     }
-
-
 }
