c41a1e565f10bebaa2ace692a2dd041cbbdbb5d9
alexskv4 <alexskv4@gmail.com>
2022-03-31 11:08:56 -0700

    Added enter key functionality to TextfieldSudokuLoader and reformatted some of the code for it.

--- src/main/java/sudokusolverjavafx/TextfieldSudokuLoader.java
+++ src/main/java/sudokusolverjavafx/TextfieldSudokuLoader.java
@@ -2,6 +2,7 @@ package sudokusolverjavafx;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.HBox;
 
 public class TextfieldSudokuLoader extends HBox {
@@ -10,17 +11,26 @@ public class TextfieldSudokuLoader extends HBox {
         TextField textField = new TextField("Enter Sudoku String");
         Button submitButton = new Button("Load");
 
+        textField.setOnKeyPressed(event -> {
+            if(event.getCode() == KeyCode.ENTER){
+                loadSudoku(grid, textField);
+            }
+        });
+
         submitButton.setOnAction(event -> {
-            try {
-                grid.loadSudokuFromString(textField.getText());
-            }
-            catch (NumberFormatException e){
-                textField.setText("Not a valid format.");
-            }
+            loadSudoku(grid, textField);
         });
 
         getChildren().add(textField);
         getChildren().add(submitButton);
+    }
 
+    private void loadSudoku (SudokuGrid grid, TextField textField){
+        try {
+            grid.loadSudokuFromString(textField.getText());
+        }
+        catch (NumberFormatException e){
+            textField.setText("Not a valid format.");
+        }
     }
 }
